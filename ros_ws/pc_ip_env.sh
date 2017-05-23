af2c1dc3e911bcccff54d3b6df969a4e5fc4ae97
Romain Reignier <rom.reignier@gmail.com>
2017-05-23 17:05:48 +0200

    Add env config scripts.

---
+++ ros_ws/pc_ip_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source devel/setup.bash
+export ROS_MASTER_URI=http://10.42.0.1:11311
+export ROS_HOSTNAME=10.42.0.11
+export ROS_IP=10.42.0.11
---
+++ ros_ws/robot_ip_env.sh
@@ -0,0 +1,4 @@
+source devel/setup.bash
+export ROS_MASTER_URI=http://10.42.0.1:11311
+export ROS_HOSTNAME=10.42.0.1
+export ROS_IP=10.42.0.1
