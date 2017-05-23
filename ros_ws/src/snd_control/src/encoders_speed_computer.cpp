4a4f28eca79c98d2ad1916684c6ee0a403c7b4c7
Romain Reignier <rom.reignier@gmail.com>
2017-05-23 17:19:03 +0200

    Add a small node to compute encoders speed.

---
+++ ros_ws/src/snd_control/src/encoders_speed_computer.cpp
@@ -0,0 +1,32 @@
+#include <ros/ros.h>
+
+#include <snd_msgs/Encoders.h>
+#include <snd_msgs/Motors.h>
+
+ros::Publisher speedPub;
+snd_msgs::EncodersConstPtr lastMsg;
+
+void encodersCb(const snd_msgs::EncodersConstPtr& _msg)
+{
+  if(lastMsg)
+  {
+    const ros::Duration dt = _msg->header.stamp - lastMsg->header.stamp;
+    snd_msgs::Motors speed;
+    speed.header = _msg->header;
+    speed.left = (_msg->left - lastMsg->left) / dt.toSec();
+    speed.right = (_msg->right - lastMsg->right) / dt.toSec();
+    speedPub.publish(speed);
+  }
+  lastMsg = _msg;
+}
+
+int main(int _argc, char** _argv)
+{
+    ros::init(_argc, _argv, "encoders_speed_computer");
+    ROS_INFO("Ready to compute the speed from topic /encoders on /encoders_speed");
+    ros::NodeHandle nh;
+    ros::Subscriber encodersSub = nh.subscribe("/encoders", 1, encodersCb);
+    speedPub = nh.advertise<snd_msgs::Motors>("/encoders_speed", 10);
+    ros::spin();
+    return 0;
+}
