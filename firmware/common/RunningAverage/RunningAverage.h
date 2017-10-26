d390623f599ed2a13dc1149daf4c3a8501f1ad48
Romain Reignier <rom.reignier@gmail.com>
2017-10-26 13:55:43 +0200

    Bugfix RunningAverage

--- firmware/common/RunningAverage/RunningAverage.h
+++ firmware/common/RunningAverage/RunningAverage.h
@@ -5,24 +5,28 @@
 template <typename T, size_t S> class RunningAverage
 {
 public:
-    void add(T _value)
-    {
-        m_sum -= m_array[m_index];
-        m_array[m_index] = _value;
-        m_sum += m_array[m_index];
-        m_index++;
-        if(m_index == S) m_index = 0;
-        if(m_count < S) m_count++;
-    }
+  RunningAverage()
+  {
+    m_array.fill(0);
+  }
 
-    T getAverage() const { return m_sum / m_count; }
+  void add(T _value)
+  {
+    m_sum -= m_array[m_index];
+    m_array[m_index] = _value;
+    m_sum += m_array[m_index];
+    m_index++;
+    if(m_index == S) m_index = 0;
+    if(m_count < S) m_count++;
+  }
 
-    size_t getCount() const { return m_count; }
+  float getAverage() const { return m_sum / static_cast<float>(m_count); }
+
+  size_t getCount() const { return m_count; }
 
 private:
-    const size_t m_size = S;
-    size_t m_index = 0;
-    size_t m_count = 0;
-    T m_sum = 0;
-    std::array<T, S> m_array;
+  size_t m_index = 0;
+  size_t m_count = 0;
+  T m_sum = 0;
+  std::array<T, S> m_array;
 };
