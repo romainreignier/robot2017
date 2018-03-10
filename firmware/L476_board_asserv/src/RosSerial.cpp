0cc7055c15f6e0c41584d47aab67f45ea1a58169
David Delacre <delacre.david@nuxibox.com>
2018-03-10 23:43:47 +0100

    refactor code and adding somes news messages

--- firmware/L476_board_asserv/src/RosSerial.cpp
+++ firmware/L476_board_asserv/src/RosSerial.cpp
@@ -6,121 +6,130 @@
 #include <ros.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/Int16.h>
+#include <snd_msgs/Encoders.h>
+#include <snd_msgs/GlobalStatus.h>
+#include <snd_msgs/Feedback.h>
 #include <geometry_msgs/Pose2D.h>
 
 
-
+static ros::NodeHandle s_NodeHandle;
 
 void CallbackDeplacement(const geometry_msgs::Pose2D & msg)
 {
     GoingToPoint(msg.x,msg.y);
 }
+
+
+void CopyDataOnMessage_Encodeurs(snd_msgs::Encoders & pEncodeurs){
+    pEncodeurs.header.stamp         = s_NodeHandle.now();
+
+    pEncodeurs.left                 = gBoard.tickg;
+    pEncodeurs.right                = gBoard.tickr;
+}
+
+
+void CopyDataOnMessage_GlobalStatus(snd_msgs::GlobalStatus & pGlobalStatus){
+    pGlobalStatus.header.stamp      = s_NodeHandle.now();
+
+    pGlobalStatus.starter           = gBoard.starter.read();
+
+    pGlobalStatus.position.x        = gBoard.G_X_mm;
+    pGlobalStatus.position.y        = gBoard.G_Y_mm;
+    pGlobalStatus.position.theta    = gBoard.G_Theta_rad;
+
+    pGlobalStatus.speed             = gBoard.linear_speed;
+
+}
+
+
+void CopyDataOnMessage_Feedback(snd_msgs::Feedback & pFeedback){
+    pFeedback.header.stamp          = s_NodeHandle.now();
+
+    pFeedback.consigne_distance     = gBoard.consigneDistance;
+    pFeedback.consigne_angle        = gBoard.consigneAngle;
+    pFeedback.mesure_distance       = gBoard.mesureDistance;
+    pFeedback.mesure_angle          = gBoard.mesureAngle;
+    pFeedback.erreur_distance       = gBoard.erreurDistance;
+    pFeedback.erreur_angle          = gBoard.erreurAngle;
+    pFeedback.cible_distance        = gBoard.cibleDistance;
+    pFeedback.cible_angle           = gBoard.cibleAngle;
+    pFeedback.left_speed            = gBoard.leftSpeed;
+    pFeedback.right_speed           = gBoard.rightSpeed;
+    pFeedback.left_pwm              = gBoard.leftPwm;
+    pFeedback.right_pwm             = gBoard.rightPwm;
+    pFeedback.smooth_rotation       = gBoard.smoothRotation;
+}
+
+
 THD_WORKING_AREA(waThreadRosserial, 2048);
 THD_FUNCTION(ThreadRosserial, arg)
 {
-  (void)arg;
   chRegSetThreadName("rosserial");
 
+  (void)arg;
+
+  int _modulo           = 0;
+  systime_t time        = chVTGetSystemTimeX();
+  constexpr systime_t   kPublishPeriodMs{10};
+
   // ROS
-  ros::NodeHandle nh;
   //ros::NodeHandle_<ChibiOSHardware,25,25,512,512> nh;
   // Messages
-  std_msgs::Float32 consigne_distance;
-  std_msgs::Float32 consigne_angle;
-  std_msgs::Float32 mesure_distance;
-  std_msgs::Float32 mesure_angle;
-  std_msgs::Float32 erreur_distance;
-  std_msgs::Float32 erreur_angle;
-  std_msgs::Float32 cible_distance;
-  std_msgs::Float32 cible_angle;
-  std_msgs::Float32 left_speed;
-  std_msgs::Float32 right_speed;
-  std_msgs::Float32 smoothRotation;
-  std_msgs::Float32 linear_speed;
-  std_msgs::Int16 left_pwm;
-  std_msgs::Int16 right_pwm;
-  // Publishers
-  ros::Publisher consigne_distance_pub("consigne_distance", &consigne_distance);
-  ros::Publisher consigne_angle_pub("consigne_angle", &consigne_angle);
-  ros::Publisher mesure_distance_pub("mesure_distance", &mesure_distance);
-  ros::Publisher mesure_angle_pub("mesure_angle", &mesure_angle);
-  ros::Publisher erreur_distance_pub("erreur_distance", &erreur_distance);
-  ros::Publisher erreur_angle_pub("erreur_angle", &erreur_angle);
-  ros::Publisher cible_distance_pub("cible_distance", &cible_distance);
-  ros::Publisher cible_angle_pub("cible_angle", &cible_angle);
-  ros::Publisher left_speed_pub("left_speed", &left_speed);
-  ros::Publisher right_speed_pub("right_speed", &right_speed);
-  ros::Publisher right_pwm_pub("right_pwm", &right_pwm);
-  ros::Publisher left_pwm_pub("left_pwm", &left_pwm);
-  ros::Publisher smooth_rotation_pub("smooth_rotation", &smoothRotation);
-  ros::Publisher linear_speed_pub("linear_speed", &linear_speed);
+  snd_msgs::Encoders        msg_Encodeurs;
+  snd_msgs::GlobalStatus    msg_GlobalStatus;
+  snd_msgs::Feedback        msg_Feedback;
 
+  // Publishers
+  ros::Publisher msg_Encodeurs_pub      ("Encodeurs",       &msg_Encodeurs);
+  ros::Publisher msg_GlobalStatus_pub   ("GlobalStatus",    &msg_GlobalStatus);
+  ros::Publisher msg_Feedback_pub       ("Feedback",        &msg_Feedback);
 
   // Subscribers
   ros::Subscriber <geometry_msgs::Pose2D>
           consigne_deplacement_sub("consigne_deplacement", CallbackDeplacement);
 
   //Init Subscribers
-  nh.subscribe(consigne_deplacement_sub);
+  s_NodeHandle.subscribe(consigne_deplacement_sub);
 
   // Init Node Handle
-  nh.getHardware()->setDriver(&SD1);
-  nh.initNode();
-  // Init Publishers
-  nh.advertise(consigne_distance_pub);
-  nh.advertise(consigne_angle_pub);
-  nh.advertise(mesure_distance_pub);
-  nh.advertise(mesure_angle_pub);
-  nh.advertise(erreur_distance_pub);
-  nh.advertise(erreur_angle_pub);
-  nh.advertise(cible_distance_pub);
-  nh.advertise(cible_angle_pub);
-  nh.advertise(left_speed_pub);
-  nh.advertise(right_speed_pub);
-  nh.advertise(right_pwm_pub);
-  nh.advertise(left_pwm_pub);
-  nh.advertise(smooth_rotation_pub);
-  nh.advertise(linear_speed_pub);
+  s_NodeHandle.getHardware()->setDriver(&SD1);
+  s_NodeHandle.initNode();
 
-  systime_t time = chVTGetSystemTimeX();
-  constexpr systime_t kPublishPeriodMs{50};
+  // Init Publishers
+  s_NodeHandle.advertise(msg_Encodeurs_pub);
+  s_NodeHandle.advertise(msg_GlobalStatus_pub);
+  s_NodeHandle.advertise(msg_Feedback_pub);
+
   while(true)
   {
     time += MS2ST(kPublishPeriodMs);
 
-        // Copy the values
-        consigne_distance.data = gBoard.consigneDistance;
-        consigne_angle.data = gBoard.consigneAngle;
-        mesure_distance.data = gBoard.mesureDistance;
-        mesure_angle.data = gBoard.mesureAngle;
-        erreur_distance.data = gBoard.erreurDistance;
-        erreur_angle.data = gBoard.erreurAngle;
-        cible_distance.data = gBoard.cibleDistance;
-        cible_angle.data = gBoard.cibleAngle;
-        left_speed.data = gBoard.leftSpeed;
-        right_speed.data = gBoard.rightSpeed;
-        left_pwm.data = gBoard.leftPwm;
-        right_pwm.data = gBoard.rightPwm;
-        smoothRotation.data = gBoard.smoothRotation;
-        linear_speed.data = gBoard.linear_speed;
+    if(_modulo % 2)// toutes les 20MS
+    {
+        /*** Copy Data ***/
+         CopyDataOnMessage_Feedback(msg_Feedback);
 
-        // Publish the data
-        consigne_distance_pub.publish(&consigne_distance);
-        consigne_angle_pub.publish(&consigne_angle);
-        mesure_distance_pub.publish(&mesure_distance);
-        mesure_angle_pub.publish(&mesure_angle);
-        erreur_distance_pub.publish(&erreur_distance);
-        erreur_angle_pub.publish(&erreur_angle);
-        cible_distance_pub.publish(&cible_distance);
-        cible_angle_pub.publish(&cible_angle);
-        left_speed_pub.publish(&left_speed);
-        right_speed_pub.publish(&right_speed);
-        right_pwm_pub.publish(&right_pwm);
-        left_pwm_pub.publish(&left_pwm);
-        smooth_rotation_pub.publish(&smoothRotation);
-        linear_speed_pub.publish(&linear_speed);
+         /*** Publish ***/
+         msg_Feedback_pub.publish(&msg_Feedback);
+    }
+
+    if(_modulo % 50)// toutes les 500MS
+    {
+       /*** Copy Data ***/
+        CopyDataOnMessage_Encodeurs(msg_Encodeurs);
+        CopyDataOnMessage_GlobalStatus(msg_GlobalStatus);
+
+        /*** Publish ***/
+        msg_Encodeurs_pub.publish(&msg_Encodeurs);
+        msg_GlobalStatus_pub.publish(&msg_GlobalStatus);
+    }
+
+    if(_modulo >= 1000)//Reset if > 10sec
+        _modulo=0;
+    else
+        _modulo++;
     // Spin
-    nh.spinOnce();
+    s_NodeHandle.spinOnce();
     chThdSleepUntil(time);
   }
 }
